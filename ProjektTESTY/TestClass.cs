19869f2b5cccc0fd382c790d6cd660dac4509a5e
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2022-08-28 17:41:26 +0200

    Tutorial1

---
+++ ProjektTESTY/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using ProjektTESTY.BaseClass;
+using System.Threading;
+
+namespace ProjektTESTY
+{
+    [TestFixture]
+    public class TestClass : BaseTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            IWebElement emailField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailField.SendKeys("Mateusz");
+            Thread.Sleep(5000);
+        }
+    }
+}
