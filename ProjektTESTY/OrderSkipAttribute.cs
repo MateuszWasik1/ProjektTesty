151d8e6e7c552453e9e4d576b9fd40c1be969acb
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2022-08-29 17:41:46 +0200

    Tutorial - 3'rd video code

---
+++ ProjektTESTY/OrderSkipAttribute.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjektTESTY
+{
+    [TestFixture]
+
+    public class OrderSkipAttribute
+    {
+        [Test,Order(1), Category("TutorialTest")]
+        public void TestChrome()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.facebook.com/";
+            IWebElement emailField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailField.SendKeys("Mateusz");
+            Thread.Sleep(5000);
+            driver.Close();
+        }
+        [Test, Order(0),Category("TutorialTest")]
+        public void TestFirefox()
+        {
+            Assert.Ignore("Lack of Browser"); //Ignorowanie Testu
+
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.facebook.com/";
+            IWebElement emailField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailField.SendKeys("Mateusz");
+            Thread.Sleep(5000);
+            driver.Close();
+        }
+        [Test, Order(2), Category("TutorialTest")]
+        public void TestIE()
+        {
+            Assert.Ignore(); //Ignorowanie Testu
+
+            IWebDriver driver = new InternetExplorerDriver();
+            driver.Url = "https://www.facebook.com/";
+            IWebElement emailField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailField.SendKeys("Mateusz");
+            Thread.Sleep(5000);
+            driver.Close();
+        }
+    }
+}
---
+++ ProjektTESTY/ParallelTesting.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using ProjektTESTY.BaseClass;
+using ProjektTESTY.Utilities;
+using System.Threading;
+
+namespace ProjektTESTY
+{
+    [TestFixture]
+    public class ParallelTesting
+    {
+        IWebDriver driver;
+        [Test, Category("TutorialTest"), Category("Module1")]
+        public void TestMethod()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+            IWebElement emailField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailField.SendKeys("Mateusz");
+            Driver.Close();
+        }
+    }
+}
